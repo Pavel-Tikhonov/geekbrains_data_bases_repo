55375ab65db0c903119d03006960101cf315cd2a
Pavel-Tikhonov <tishkaas@gmail.com>
2020-06-16 21:47:51 +0300

    Дз к уроку 2.

---
+++ lesson2_task2.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS example;
+USE example;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+SHOW TABLES;
+INSERT INTO users VALUES(NULL, 'pavel';)
+SELECT * FROM users;
+
